ed0f3939259c67bb745053426c461a0381cceb21
Adhijaan <adnanr@umich.edu>
2024-11-17 17:01:49 -0500

    Linked start to form page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
       <div className="d-flex justify-content-center align-items-center h-100">
-        <button className="btn btn-lg bg-secondary w-50 fs-1 fw-bold" style={{ height: "10vh" }}>
+        <Link href="/form" className="btn btn-lg bg-secondary w-50 fs-1 fw-bold" style={{ height: "10vh" }}>
           Start
-        </button>
+        </Link>
       </div>
     </>
   );
