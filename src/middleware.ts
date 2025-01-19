4e331624a3b95f1ebf28859f5d2bde68ced9783a
Adhijaan <adnanr@umich.edu>
2025-01-18 21:29:35 -0500

    Google auth flow working

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,69 +1,19 @@
-// src/middleware.ts
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  let response = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
-  });
-
-  // Create a Supabase client for the middleware
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value;
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          });
-        },
-        remove(name: string, options: CookieOptions) {
-          response.cookies.set({
-            name,
-            value: "",
-            ...options,
-          });
-        },
-      },
-    }
-  );
-
-  // Get the session from Supabase
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  if (session) {
-    console.log("User is logged in:", session.user.email);
-  } else {
-    console.log("No active session. User is not logged in.");
-  }
-
-  const path = request.nextUrl.pathname;
-
-  // If the user is not authenticated and the route is not /login, redirect to /login
-  if (!session && path !== "/login" && path !== "/logout") {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  // If the user is authenticated and the route is /login, redirect to /
-  if (session && path === "/login") {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  return response;
+  return await updateSession(request);
 }
 
-// Routes Middleware should not run on
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|logout).*)"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
 };
